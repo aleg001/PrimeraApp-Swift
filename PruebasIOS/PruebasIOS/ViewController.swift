661d3eabace7895b46fb20aac38b3f821abfeb2e
Ale Gómez <gom20347@uvg.edu.gt>
2021-12-10 23:17:49 -0600

    Primera aplicación desarrollada en Swift
    Generador de fotos aleatorias implementando funcionalidades básicas para cambiar fondo de color y mostrar texto.

---
+++ PruebasIOS/PruebasIOS/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  PruebasIOS
+//
+//  Created by Alejandro Gomez on 10/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .systemCyan
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Siguiente foto aleatoria",for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitleShadowColor(.green, for: .normal)
+        return button
+    }()
+    
+    let arrayColores: [UIColor] = [
+        .systemCyan,
+        .systemMint,
+        .red,
+        .blue,
+        .lightGray,
+        .magenta,
+        .purple,
+        .yellow,
+        .systemTeal
+    ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+        imageView.center = view.center
+        
+        view.addSubview(button)
+
+        getPhoto()
+        button.addTarget(self, action: #selector(ApacharBoton), for: .touchUpInside)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        button.frame = CGRect(x: 0, y: view.frame.size.height-80, width: view.frame.size.width, height: 40)
+    }
+    
+    @objc func ApacharBoton(){
+        getPhoto()
+        view.backgroundColor = arrayColores.randomElement()
+    }
+
+    func getPhoto (){
+        let urlPagina = "https://picsum.photos/200"
+        let urlPhoto = URL(string: urlPagina)!
+        guard let data = try? Data(contentsOf: urlPhoto) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
+    
+   
+}
+
---
+++ PruebasIOS/PruebasIOSTests/PruebasIOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  PruebasIOSTests.swift
+//  PruebasIOSTests
+//
+//  Created by Alejandro Gomez on 10/12/21.
+//
+
+import XCTest
+@testable import PruebasIOS
+
+class PruebasIOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
